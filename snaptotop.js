3c1fd5d6c82774087220c9b86cc72708ed866691
Andreas Gade Jensen <andreas_gade_jensen@hotmail.com>
2015-07-10 14:21:52 +0200

    Added vanish and distance top/bottom

--- snaptotop.js
+++ snaptotop.js
@@ -7,8 +7,12 @@
 	    replacementClass: 'snap-to-top-replacement',
 	    activeClass: 'fixed-snap',
 	    distance_top: 20,
+	    distance_bottom: 20,
 	    animation: true,
 	    animationDuration: 500,
+	    atStart: function(element){},
+	    atEndVanish: true,
+		atEnd: function(element){},
 	    _randomMax: 1000,
 	    _randomMin: 0,
 	    _lockedClone: false,
@@ -32,11 +36,40 @@
 	    this._height = $(this.element).outerHeight();
 	    this._width = $(this.element).outerWidth();
 	    this._parent = false;	    
-	    if(jQuery(this.element).attr('data-parent')){
-			this._parent = $(this.element).attr('data-parent');
+	    if(jQuery(this.element).data('parent')){
+			this._parent = $(this.element).data('parent');
 			this._parent_top = $(this._parent).offset().top;
 		}
 		this._offset = $(this.element).offset();
+		if($(this.element).data('pos-top') && !isNaN(parseInt($(this.element).data('pos-top')))){
+			this._offset.top = parseInt($(this.element).data('pos-top'));
+		}
+		if($(this.element).data('pos-left') && !isNaN(parseInt($(this.element).data('pos-left')))){
+			this._offset.left = parseInt($(this.element).data('pos-left'));
+		}
+		if($(this).data('distance-top') && !isNaN(parseInt($(this).data('distance-top')))){
+			this.distance_top = parseInt($(this).data('distance-top'));
+		}
+		if($(this).data('distance-bottom') && !isNaN(parseInt($(this).data('distance-bottom')))){
+			this.distance_bottom = parseInt($(this).data('distance-bottom'));
+		}
+		if($(this).data('animation')){
+			if($(this).data('animation') == "1" && $(this).data('animation') == 1){
+				this.animation = true;
+			} else {
+				this.animation = false;
+			}
+			if($(this).data('animation-duration') && !isNaN(parseInt($(this).data('animation-duration')))){
+				this.animationDuration = parseInt($(this).data('animation-duration'));
+			}
+		}
+		if($(this).data('vanish')){
+			if($(this).data('vanish') == "1" && $(this).data('vanish') == 1){
+				this.atEndVanish = true;
+			} else {
+				this.atEndVanish = false;
+			}
+		}
 		this._tagName = $(this.element).prop('tagName');
 		this.options._lastY = $(window).scrollTop();
 		this.scrollFixedTop();
@@ -58,59 +91,77 @@
 		} else {
 			this.options._lastDirection = 'still';
 		}
-		console.log(this.options._lastDirection);
-		if(this._offset.top - this.options.distance_top <= window_top && (!this._parent || (this._parent_top - window_top >= (this.options.distance_top*2) + this._height))){
-			if(this.options._lockedClone){
-				/* IF THE FADE ON CLONED ELEMENT ISN'T FINISHED, THEN STOP THE FADING */
-				$(this.options._lockedClone).stop(true, true);
-			}
-			if(!$(this.element).hasClass(this.options.activeClass)){
-				this._addReplacement();
-				$(this.element).addClass(this.options.activeClass);
-				$(this.element).stop().css({
-					'width': this._width,
-					'position': 'fixed',
-					'left': this._offset.left,
-					'top': this.options.distance_top,
-				});
-				if(this.options._lastDirection == "up" && this.options.animation){
-					$(this.element).stop().hide();
-					$(this.element).fadeIn(this.options.animationDuration, function(){
-						
-					});
-				} else {
-					$(this.element).stop().show();
-				}
-			}
-		} else if(this._parent_top - window_top < (this.options.distance_top*2) + this._height) {
+		if(this._offset.top - this.options.distance_top <= window_top && (!this._parent || (this._parent_top - window_top >= (this.options.distance_top + this.options.distance_bottom) + this._height))){
+			this._addSnap();
+		} else if(this._parent_top - window_top < (this.options.distance_top + this.options.distance_bottom) + this._height) {
 			/* THE ELEMENT IS AT THE STOP ELEMENT */
 			var scrollFixedTop = this;
-			if(this.options.animation){
-				this.options._lockedClone = $(this.element).clone();
-				$(this.options._lockedClone).css({
-					'position': 'absolute',
-					'left': $(this.element).offset().left,
-					'top': $(this.element).offset().top,
-					'margin-left': 0,
-					'margin-right': 0
-				});
-				$('body').append(this.options._lockedClone);
-				$(this.options._lockedClone).stop().fadeOut(this.options.animationDuration, function(){
-					$(scrollFixedTop.options._lockedClone).remove();
-					scrollFixedTop.options._lockedClone = false;
-				});
+			if($(this.options._lockedClone).length == 0){
+				if(this.options.animation && this.options.atEndVanish){
+					this.options._lockedClone = $(this.element).clone();
+					$(this.options._lockedClone).css({
+						'position': 'absolute',
+						'left': $(this.element).offset().left,
+						'top': $(this.element).offset().top,
+						'margin-left': 0,
+						'margin-right': 0
+					});
+					$('body').append(this.options._lockedClone);
+					$(this.options._lockedClone).stop().fadeOut(this.options.animationDuration, function(){
+						$(scrollFixedTop.options._lockedClone).remove();
+						scrollFixedTop.options._lockedClone = false;
+					});
+				} else if(this.options.atEndVanish == false){
+					this.options._lockedClone = $(this.element).clone();
+					$(this.options._lockedClone).css({
+						'position': 'absolute',
+						'left': $(this.element).offset().left,
+						'top': this._parent_top - this._height - this.options.distance_bottom,
+						'margin-left': 0,
+						'margin-right': 0
+					});
+					$('body').append(this.options._lockedClone);
+				}
 			}
-			scrollFixedTop._removeReplacement();
-			$(scrollFixedTop.element).removeAttr('style');
-			$(scrollFixedTop.element).removeClass(scrollFixedTop.options.activeClass);
+			this._removeSnap();
 			
 		} else if(this._offset.top - this.options.distance_top > window_top){
 			/* IF WINDOW TOP IS AT THE CORRECT POSITION FOR THE ELEMENT, THEN JUST FIX IT, WITHOUT ANIMATION */
-			this._removeReplacement();
-			$(this.element).removeAttr('style');
-			$(this.element).removeClass(this.options.activeClass);
+			this._removeSnap();
 		}
 		this.options._lastY = window_top;
+    };
+    Plugin.prototype._removeSnap = function(){
+	    this._removeReplacement();
+		$(this.element).removeAttr('style');
+		$(this.element).removeClass(this.options.activeClass);
+		this.options.atEnd(this.element);
+    };
+    Plugin.prototype._addSnap = function(){
+	    if(this.options._lockedClone){
+			/* IF THE FADE ON CLONED ELEMENT ISN'T FINISHED, THEN STOP THE FADING */
+			$(this.options._lockedClone).stop(true, true).remove();
+			this.options._lockedClone = false;
+		}
+		if(!$(this.element).hasClass(this.options.activeClass)){
+			this.options.atStart(this.element);
+			this._addReplacement();
+			$(this.element).addClass(this.options.activeClass);
+			$(this.element).stop().css({
+				'width': this._width,
+				'position': 'fixed',
+				'left': this._offset.left,
+				'top': this.options.distance_top,
+			});
+			if(this.options._lastDirection == "up" && this.options.animation && this.options.atEndVanish){
+				$(this.element).stop().hide();
+				$(this.element).fadeIn(this.options.animationDuration, function(){
+					
+				});
+			} else {
+				$(this.element).stop().show();
+			}
+		}
     };
     Plugin.prototype._addReplacement = function(){
 	    /* THE REPLACEMENT HELPS THE CONTENT, NOT TO JUMP AROUND, WHEN THE ELEMENT IS FIXED */
